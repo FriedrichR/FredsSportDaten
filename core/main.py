9e472f9c35432b63a76608222707f521bfb5d4e6
FriedrichR <gruenehorde@hotmail.com>
2018-05-16 09:06:12 +0200

    Now runs through all exercises (and body weight)

--- core/main.py
+++ core/main.py
@@ -13,8 +13,10 @@ from pandas.core.generic import NDFrame
 data = pd.read_csv('../data/SportyFreddy.csv', delimiter=';')
 print(data.head())
 
+headers = list(data)
 data_date_str = data['Date']
-data_weight = data['Weight']
+#data_weight = data['Weight']
+
 
 #convert date strings from CSV to unix
 data_date = []
@@ -24,24 +26,29 @@ for dates in data_date_str:
     data_date.append(a.date())
     data_date_pred.append([a.timestamp()])
 
-#remove NaN-values for linear regression model
-nan = np.isnan(data_weight)
-data_weight_nonan = []
-data_date_pred_nonan = []
-for i in range(0, len(nan)):
-    if not nan[i]:
-        data_weight_nonan.append([data_weight[i]])
-        data_date_pred_nonan.append(data_date_pred[i])
+for header in headers[1:]:
+    print(header)
 
-#linear regression
-model = LinearRegression()
-model.fit(data_date_pred_nonan, data_weight_nonan)
-predicted = model.predict(data_date_pred)
+    data_values = data[header]
 
-#plot
-plt.plot(data_date, data_weight, 'bo')
-plt.plot(data_date, predicted)
-plt.xlabel('Date')
-plt.ylabel('Weight [kg]')
-plt.title('Weight')
-plt.show()
+    #remove NaN-values for linear regression model
+    nan = np.isnan(data_values)
+    data_values_nonan = []
+    data_date_pred_nonan = []
+    for i in range(0, len(nan)):
+        if not nan[i]:
+            data_values_nonan.append([data_values[i]])
+            data_date_pred_nonan.append(data_date_pred[i])
+
+    #linear regression
+    model = LinearRegression()
+    model.fit(data_date_pred_nonan, data_values_nonan)
+    predicted = model.predict(data_date_pred)
+
+    #plot
+    plt.plot(data_date, data_values, 'bo', ms=10.0, mfc='xkcd:grey', mec='xkcd:grey')
+    plt.plot(data_date, predicted,  linewidth=4.0)
+    plt.xlabel('Date', size='x-large')
+    plt.ylabel(header + ' [kg]', size='x-large')
+    plt.title(header, size='x-large')
+    plt.show()
